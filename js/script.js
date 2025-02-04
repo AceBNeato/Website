f345782083f2ec28b3193367f186f293772f529f
GIAN GARCIA <giangarcia977@gmail.com>
2025-02-04 17:05:46 +0800

    Update Account.html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+        document.getElementById('get-started-btn').addEventListener('click', () => {
+            window.location.href = "/account.html";
+        });
+        
+        document.getElementById('account-btn').addEventListener('click', () => {
+            window.location.href = "/account.html";
+        });
+
+
+    });
+ 
+      
+document.getElementById('shop-now-btn').addEventListener('click', () => {
+    window.location.href = "/cart.html";
+});
